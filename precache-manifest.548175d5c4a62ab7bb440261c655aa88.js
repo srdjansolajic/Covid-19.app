90e3b74bd358c7da1e4027eb468cb209b3dc7568
srdjansolajic <solajic.srdjan@gmail.com>
2020-09-25 12:02:40 +0200

    Updates

---
+++ precache-manifest.548175d5c4a62ab7bb440261c655aa88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2e65903342465a571573f21edf692b",
+    "url": "/Covid-19.app/index.html"
+  },
+  {
+    "revision": "e1d9b4cc41f24064aa92",
+    "url": "/Covid-19.app/static/css/main.8f94baf6.chunk.css"
+  },
+  {
+    "revision": "c6e0da0ed903b47073d2",
+    "url": "/Covid-19.app/static/js/2.6347e587.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid-19.app/static/js/2.6347e587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d9b4cc41f24064aa92",
+    "url": "/Covid-19.app/static/js/main.7dee2003.chunk.js"
+  },
+  {
+    "revision": "5c70f12f3c36f24214df",
+    "url": "/Covid-19.app/static/js/runtime-main.34570eff.js"
+  }
+]);
